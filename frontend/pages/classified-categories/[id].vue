0875a506d0765738d0f5be8f2a828df34be89497
asad5072 <mmonir5072@gmail.com>
2024-12-07 13:30:59 +0600

    rafsun

--- frontend/pages/classified-categories/[id].vue
+++ frontend/pages/classified-categories/[id].vue
@@ -40,61 +40,63 @@
         </UButtonGroup>
       </div>
       <div class="services mt-4">
-        <UCard
-          :ui="{
-            background: '',
-            ring: '',
-            shadow: '',
-            rounded: '',
-            body: {
-              padding: 'p-0 sm:p-0 flex-1 w-full',
-            },
-            header: {
-              padding: 'p-0',
-            },
-            footer: {
-              padding: 'p-0',
-            },
-          }"
-          class="service-card border even:border-t-0 even:border-b-0 bg-slate-50/70"
-          v-for="(service, i) in services"
-          :key="{ i }"
-        >
-          <div
-            class="flex flex-col px-3 py-2.5 sm:flex-row sm:items-center w-full"
+        <NuxtLink>
+          <UCard
+            :ui="{
+              background: '',
+              ring: '',
+              shadow: '',
+              rounded: '',
+              body: {
+                padding: 'p-0 sm:p-0 flex-1 w-full',
+              },
+              header: {
+                padding: 'p-0',
+              },
+              footer: {
+                padding: 'p-0',
+              },
+            }"
+            class="service-card border even:border-t-0 even:border-b-0 bg-slate-50/70"
+            v-for="(service, i) in services"
+            :key="{ i }"
           >
-            <div class="flex justify-between">
-              <div class="flex gap-4">
-                <div>
-                  <NuxtImg
-                    src="https://placehold.co/400"
-                    class="size-14 rounded-md"
-                  />
-                </div>
-                <div>
-                  <h3 class="text-base font-bold mb-1.5">
-                    Lorem ipsum dolor sit amet.
-                  </h3>
+            <div
+              class="flex flex-col px-3 py-2.5 sm:flex-row sm:items-center w-full"
+            >
+              <div class="flex justify-between">
+                <div class="flex gap-4">
+                  <div>
+                    <NuxtImg
+                      src="https://placehold.co/400"
+                      class="size-14 rounded-md"
+                    />
+                  </div>
+                  <div>
+                    <h3 class="text-base font-bold mb-1.5">
+                      Lorem ipsum dolor sit amet.
+                    </h3>
 
-                  <div class="flex gap-4">
-                    <p class="inline-flex gap-1 items-center">
-                      <UIcon name="i-heroicons-map-pin-solid" />
-                      <span class="text-sm">Chewriya, Kushtia</span>
-                    </p>
-                    <p class="inline-flex gap-1 items-center">
-                      <UIcon name="i-tabler:category-filled" />
-                      <span class="text-sm">Category</span>
-                    </p>
-                    <p class="inline-flex gap-1 items-center">
-                      <UIcon name="i-heroicons-clock-solid" />
-                      <span class="text-sm">Posted: 1 Day Ago</span>
-                    </p>
+                    <div class="flex gap-4">
+                      <p class="inline-flex gap-1 items-center">
+                        <UIcon name="i-heroicons-map-pin-solid" />
+                        <span class="text-sm">Chewriya, Kushtia</span>
+                      </p>
+                      <p class="inline-flex gap-1 items-center">
+                        <UIcon name="i-tabler:category-filled" />
+                        <span class="text-sm">Category</span>
+                      </p>
+                      <p class="inline-flex gap-1 items-center">
+                        <UIcon name="i-heroicons-clock-solid" />
+                        <span class="text-sm">Posted: 1 Day Ago</span>
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        </UCard>
+          </UCard>
+        </NuxtLink>
       </div>
     </UContainer>
   </PublicSection>
