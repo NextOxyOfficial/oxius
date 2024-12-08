26491ea4a5785b6daff98e79948a199772e4cc46
asad5072 <mmonir5072@gmail.com>
2024-12-08 16:07:55 +0600

    rafsun

---
+++ frontend/pages/deposit-withdraw.vue
@@ -0,0 +1,117 @@
+<template>
+  <UContainer>
+    <h1 class="text-center text-4xl my-8">Deposit/Withdraw</h1>
+    <UDivider label="" class="mb-8" />
+    <div class="flex items-center justify-between">
+      <p
+        class="text-lg bg-green-100 border-green-500 border py-2 max-w-72 w-full px-3 rounded-md mb-3 text-green-800 font-bold"
+      >
+        Available Balance: 600taka
+      </p>
+      <div class="text-lg">
+        <p
+          class="text-lg bg-green-100 border-green-500 border py-2 max-w-72 w-full px-3 rounded-md mb-3 text-green-800 font-bold"
+        >
+          Total Earnings: 500taka
+        </p>
+      </div>
+    </div>
+    <div class="flex items-center">
+      <div>
+        <div class="space-y-2">
+          <UInput
+            placeholder="Enter amount"
+            size="md"
+            :ui="{
+              padding: { md: 'px-3 py-2' },
+            }"
+          />
+          <UInputMenu
+            placeholder="Select payment method"
+            :options="methods"
+            size="md"
+            :ui="{
+              padding: { md: 'px-3 py-2' },
+            }"
+          />
+        </div>
+        <div class="my-2 space-x-3">
+          <UButton size="sm">Deposit</UButton>
+          <UButton color="gray" variant="solid">Withdraw</UButton>
+        </div>
+      </div>
+    </div>
+
+    <UTable :rows="statements" :columns="columns" class="my-8" :ui="{}">
+      <template #caption>
+        <caption class="text-center text-3xl font-semibold mb-4">
+          Deposit/Withdraw Statements
+        </caption>
+      </template>
+      <!-- <template v-slot:body="{ row }">
+        <tr v-for="(row, index) in statements" :key="row.id">
+          <td>{{ row.id }}</td>
+          <td>{{ row.time }}</td>
+          <td>{{ row.deposit_withdraw }}</td>
+          <td>{{ row.amount }}</td>
+          <td :class="getStatusClass(row.status)">
+            {{ row.status }}
+          </td>
+        </tr>
+      </template> -->
+    </UTable>
+  </UContainer>
+</template>
+
+<script setup>
+const methods = ['bkash', 'nagad', 'rocket', 'upay'];
+const columns = [
+  {
+    key: 'id',
+    label: '#ID',
+  },
+  {
+    key: 'time',
+    label: 'Time',
+  },
+  {
+    key: 'deposit_withdraw',
+    label: 'Deposit/Withdraw',
+  },
+  {
+    key: 'amount',
+    label: 'Amount',
+  },
+  {
+    key: 'status',
+    label: 'Status',
+  },
+];
+
+const statements = [
+  {
+    id: 1,
+    time: '2:35',
+    deposit_withdraw: 'Deposit',
+    amount: '300',
+    status: 'Pending',
+    class: 'text-yellow-400',
+  },
+  {
+    id: 2,
+    time: '2:35',
+    deposit_withdraw: 'Withdraw',
+    amount: '300',
+    status: 'Completed',
+  },
+
+  // function getStatusClass(status) {
+  //   if (status === 'Pending') {
+  //     return 'text-yellow-400'; // Yellow for Pending
+  //   } else if (status === 'Completed') {
+  //     return 'text-green-500'; // Green for Completed
+  //   }
+  //   return ''; // Default class
+  // },
+];
+</script>
