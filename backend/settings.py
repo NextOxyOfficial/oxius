6c38f1b9b99c6266061defd7e72eff7da5dce8e1
asad5072 <mmonir5072@gmail.com>
2024-12-15 17:05:50 +0600

    rafsun

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-!fhx-*zsy791frr7y538j7bt5mx_*5pr@*inb$w!bxzszqs0^-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['adsyclub.com', 'www.adsyclub.com']
+ALLOWED_HOSTS = ['adsyclub.com', 'www.adsyclub.com', 'localhost']
 AUTH_USER_MODEL = 'base.User'
 
 
@@ -48,16 +48,16 @@ INSTALLED_APPS = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-   'http://localhost:3000',
-   'http://localhost:3001',
-   'http://adsyclub.com',
-   'https://adsyclub.com',
-   'http://www.adsyclub.com',
-   'https://www.adsyclub.com'
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'http://adsyclub.com',
+    'https://adsyclub.com',
+    'http://www.adsyclub.com',
+    'https://www.adsyclub.com'
 ]
 
 REST_FRAMEWORK = {
-     # Permissions: Define who can access your APIs
+    # Permissions: Define who can access your APIs
     # 'DEFAULT_PERMISSION_CLASSES': [
     #     'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
     # ],
