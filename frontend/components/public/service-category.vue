634cab872fbe810bcb6e7854bef1e0f4c1038006
rafsun-riad <mruhaquer@gmail.com>
2025-03-23 11:50:18 +0600

    css fix

--- frontend/components/public/service-category.vue
+++ frontend/components/public/service-category.vue
@@ -745,9 +745,7 @@ const getTextAnimation = (id) => {
 
 .search-pulse {
   position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+
   width: 60px;
   height: 60px;
   border-radius: 50%;
