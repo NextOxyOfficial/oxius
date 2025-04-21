c9dce099127225ffaeae400f2a577260fedf64f4
ChandanSayed <hangzhou635@gmail.com>
2025-04-21 17:41:25 +0600

    News Post Urls

--- backend/urls.py
+++ backend/urls.py
@@ -27,7 +27,7 @@ urlpatterns = urlpatterns + [
     path('api/', include('mobile_recharge.urls')),
     path('api/', include('subscription.urls')),
     path('api/', include('business_network.urls')),
-    path('api/', include('news.urls')),
+    path('api/news/', include('news.urls')),
     # for frontend
     path('', index, name='index'),
     path('<str:param>', index, name='index2'),
---
+++ news/serializers.py
@@ -0,0 +1,45 @@
+from rest_framework import serializers
+from .models import *
+from base.serializers import UserSerializer
+
+class NewsMediaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NewsMedia
+        fields = '__all__'
+
+class NewsPostTagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NewsPostTag
+        fields = '__all__'
+
+class NewsPostCommentSerializer(serializers.ModelSerializer):
+    author_details = UserSerializer(source='author', read_only=True)
+    
+    class Meta:
+        model = NewsPostComment
+        fields = '__all__'
+
+class NewsPostListSerializer(serializers.ModelSerializer):
+    author_details = UserSerializer(source='author', read_only=True)
+    post_media = NewsMediaSerializer(many=True, read_only=True)
+    post_tags = NewsPostTagSerializer(many=True, read_only=True)
+    comment_count = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = NewsPost
+        fields = '__all__'
+        read_only_fields = ['slug']
+    
+    def get_comment_count(self, obj):
+        return obj.post_comments.count()
+
+class NewsPostDetailSerializer(serializers.ModelSerializer):
+    author_details = UserSerializer(source='author', read_only=True)
+    post_media = NewsMediaSerializer(many=True, read_only=True)
+    post_tags = NewsPostTagSerializer(many=True, read_only=True)
+    post_comments = NewsPostCommentSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = NewsPost
+        fields = '__all__'
+        read_only_fields = ['slug']
