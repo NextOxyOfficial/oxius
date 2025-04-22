07b18e199cf7f092b408de523ae7887a462ccf6e
alimullcz <nextoxyofficial@gmail.com>
2025-04-23 00:03:48 +0600

    ssss

--- frontend/components/adsy-news/header.vue
+++ frontend/components/adsy-news/header.vue
@@ -144,7 +144,7 @@
             </div>
           </div>
         </div>
-        <div class="flex items-center px-6 pb-4 pt-2 sm:hidden">
+        <div class="flex items-center px-6 mb-4 mt-2 sm:hidden">
           <nav
             class="flex md:ml-10 md:hidden justify-between space-x-2"
             v-if="categories?.length > 0"
