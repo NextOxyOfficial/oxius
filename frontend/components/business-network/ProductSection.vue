4f144f25a80e3bed354776caeaf8f4398a4f9672
adsy <adsy@mail.com>
2025-04-30 01:22:25 +0600

    check2

--- frontend/components/business-network/ProductSection.vue
+++ frontend/components/business-network/ProductSection.vue
@@ -3,7 +3,7 @@
     <h2 class="text-lg font-semibold text-gray-800 mb-4">Sponsored Products</h2>
     <div class="relative">
       <!-- Carousel Container for Mobile -->
-      <div class="carousel flex overflow-x-auto gap-2 my-2 pb-2 !sm:hidden overflow-hidden">
+      <div class="grid grid-cols-3 gap-2 my-2 pb-2 sm:hidden">
         <CommonProductCard
           v-for="(product, productIndex) in products"
           :key="`mobile-${product.id || productIndex}`"
@@ -13,7 +13,7 @@
       </div>
 
       <!-- Grid Layout for Desktop -->
-      <div class="hidden lg:grid lg:grid-cols-3 my-2 gap-4 overflow-hidden">
+      <div class="hidden lg:grid lg:grid-cols-3 my-2 gap-4">
         <CommonProductCard
           v-for="(product, productIndex) in products"
           :key="`desktop-${product.id || productIndex}`"
