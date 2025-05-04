409d95111fde16efe09146bc2badecef60101545
adsy <adsy@mail.com>
2025-05-05 00:45:31 +0600

    check 3

--- frontend/components/business-network/PostHeader.vue
+++ frontend/components/business-network/PostHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex items-center justify-between mb-3 px-2 bg-slate-50 py-3 px-2 rounded-xl">
+  <div class="flex items-center justify-between mb-3 bg-slate-50 py-3 rounded-xl">
     <div class="flex items-center space-x-3 flex-1">
       <div class="relative group">
         <NuxtLink :to="`/business-network/profile/${post.author}`">
