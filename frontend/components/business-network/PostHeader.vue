f16cf2143e4501a377348e0fa4cdd98e5a1e30e0
alimullcz <nextoxyofficial@gmail.com>
2025-05-05 14:55:41 +0600

    size fix

--- frontend/components/business-network/PostHeader.vue
+++ frontend/components/business-network/PostHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex items-center justify-between mb-3 bg-slate-50 py-3 rounded-xl">
-    <div class="flex items-center space-x-3 flex-1">
+    <div class="flex items-center space-x-3 flex-1 pl-2">
       <div class="relative group">
         <NuxtLink :to="`/business-network/profile/${post.author}`">
           <!-- Pro user badge with improved color ring around profile picture -->
