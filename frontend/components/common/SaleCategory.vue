140d880b914af9cf1dc124264afd6d11b7ccb89d
adsy <adsy@mail.com>
2025-05-22 00:25:42 +0600

    pt

--- frontend/components/common/SaleCategory.vue
+++ frontend/components/common/SaleCategory.vue
@@ -394,7 +394,7 @@
   <Teleport to="body">
     <div
       v-if="showMyPostsModal"
-      class="fixed inset-0 z-50 overflow-y-auto pt-0 md:pt-16"
+      class="fixed inset-0 z-50 overflow-y-auto pt-14 md:pt-16"
     >
       <!-- Added pt-16 for header spacing -->
       <div
@@ -411,7 +411,7 @@
           v-if="user?.user"
           class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-sm transform transition-all sm:my-8 sm:align-middle sm:max-w-4xl sm:w-full"
         >
-          <div class="bg-white px-2 pt-15 pb-4 sm:p-6 sm:pb-4">
+          <div class="bg-white px-2 pt-5 pb-4 sm:p-6 sm:pb-4">
             <div class="sm:flex sm:items-start">
               <div class="mt-3 text-center sm:mt-0 sm:text-left w-full">
                 <div
