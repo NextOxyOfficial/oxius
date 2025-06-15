61cced1ef41e0e41fb25415feab119bd67362ab0
alimullcz <nextoxyofficial@gmail.com>
2025-06-15 21:42:25 +0600

    capacitor

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,29 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.adsyclub.app',
+  appName: 'AdsyClub',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      backgroundColor: "#10b981",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: true,
+    }
+  }
+};
+
+export default config;
