5f7d468c59a2290f2f600e6de4255c7d392c89f3
adsy <adsy@mail.com>
2025-06-18 00:11:58 +0600

    adsy

--- frontend/components/common/add-product-tab.vue
+++ frontend/components/common/add-product-tab.vue
@@ -218,7 +218,7 @@
               Pricing & Inventory
             </h2>
 
-            <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+            <div class="grid grid-cols-1 sm:grid-cols-2 gap-1 lg:grid-cols-3">
               <UFormGroup
                 label="Regular Price"
                 required
