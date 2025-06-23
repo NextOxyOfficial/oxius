6bfd6c29be25a45a8c5c5280dcb257e811acd053
rafsun-riad <mruhaquer@gmail.com>
2025-06-23 13:01:50 +0600

    update

--- news/views.py
+++ news/views.py
@@ -18,7 +18,7 @@ class AdvancedPagination(LimitOffsetPagination):
 
 
 class CustomPageNumberPagination(PageNumberPagination):
-    page_size = 10
+    page_size = 12
     page_size_query_param = 'limit'
     max_page_size = 100
     page_query_param = 'page'
