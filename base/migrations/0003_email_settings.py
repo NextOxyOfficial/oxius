ec793e9b25bb2df763bd578f61d6375d6770a37f
Alimul Islam <alimulislam50@gmail.com>
2026-03-25 15:30:57 +0600

    mail

--- base/migrations/0003_email_settings.py
+++ base/migrations/0003_email_settings.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0001_initial'),
+        ('base', '0105_add_is_food_zone_to_classifiedcategory'),
     ]
 
     operations = [
